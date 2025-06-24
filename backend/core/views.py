5c681cca315978623bba3ff7e0f080175f2aad88
Divyasai Ganti <divyasaiganti@gmail.com>
2025-06-24 04:57:00 -0700

    Fix: delete-feedback url.py

--- backend/core/views.py
+++ backend/core/views.py
@@ -42,6 +42,7 @@ def get_feedbacks(request):
     data = []
     for fb in feedbacks:
         data.append({
+            "id": str(fb.id),
             "name": fb.name,
             "college_id": fb.college_id,
             "message": fb.message,
